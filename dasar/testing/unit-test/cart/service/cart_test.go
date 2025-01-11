04fd5ce03a85ee886242da9fc7289a3bca6caf7b
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2025-01-11 07:09:11 +0700

    feat: add testing example with stub and fake lib

--- dasar/testing/unit-test/cart/service/cart_test.go
+++ dasar/testing/unit-test/cart/service/cart_test.go
@@ -6,11 +6,13 @@ import (
 	"testing"
 	cart "unit-test/cart/service"
 	"unit-test/cart/service/mock"
+	"unit-test/cart/service/stub"
 
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
 
+// simulating interface on dependency with mock
 func TestShoppingCart_AddItemToCart_Success(t *testing.T){
 	//TODO: test AddItemToCart
 	ctrl := gomock.NewController(t)
@@ -32,5 +34,15 @@ func TestShoppingCart_AddItemToCart_ErrorOnRedist(t *testing.T){
 	shoppingCartService := cart.New(repositoryMock)
 	err := shoppingCartService.AddItemToCart(context.Background(), "user-2", "product-b")
 	assert.Error(t, err, "it should return any error")
+	
 
+}
+
+// simulating interface on dependency with stub
+func TestShoppingCart_AddItemToCart_Success_WithStub(t *testing.T){
+	repositoryStub := stub.NewRepositoryStub()
+	
+	shoppingCartService := cart.New(repositoryStub)
+	err := shoppingCartService.AddItemToCart(context.Background(), "user-3", "product-c")
+	assert.NoError(t, err, "it should return any error")
 }
