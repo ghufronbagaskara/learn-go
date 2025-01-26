ab32a43350fe5f3a21030ec254f60c1de8d950fe
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2025-01-26 19:46:56 +0700

    feat@CreatingTestCase: making test case part 2

---
+++ dasar/testing/unit-test/payment/repository/xendit.go
@@ -0,0 +1,73 @@
+package repository
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type XenditPayment struct{
+	host string
+	httpConnector HttpConnector
+}
+
+func NewXenditClient(httpsConnector HttpConnector, host string) XenditPayment {
+	return XenditPayment{
+		httpConnector: httpsConnector,
+		host: host,
+	}
+}
+
+type (
+	XenditPaymentReqBody struct {
+		Currency string 
+		Amount int
+		PaymentMethod PaymentMethod
+		Metadata Metadata
+	}
+
+	PaymentMethod struct {
+		PaymentMethodType string 
+		Reusability string
+		ReferenceID string
+		VirtualAccount VirtualAccount
+	}
+
+	VirtualAccount struct {
+		ChannelCode string
+		ChannelProperties ChannelProperties
+	}
+
+	ChannelProperties struct{
+		CustomerName string
+	}
+
+	Metadata struct {
+		SKU string
+	}
+)
+func (x *XenditPayment) SendPaymentRequest(ctx context.Context) (paymentID string, err error) {
+	// TODO: inject the http client
+
+	// call xendit PaymentReq API
+
+		// construct request body
+	
+	// handle error response 
+
+	// handle sucess response 
+
+	requestBody := XenditPaymentReqBody{}
+	_ = requestBody
+	reqBody := new(bytes.Buffer)
+	err = json.NewEncoder(reqBody).Encode(requestBody)
+	if err != nil {
+		return "", err
+	}
+
+
+	endpoint := fmt.Sprintf("%s%s",x.host, "/payment_requests")
+	httpReq, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, reqBody)
+}
