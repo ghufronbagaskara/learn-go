72a9a6783cdf9a08aa614cb7b89c7a733c1c5b1a
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2025-02-01 15:13:55 +0700

    feat@testing: exploring test cases and refactoring code for fetch to xendit api

--- dasar/testing/unit-test/payment/repository/xendit.go
+++ dasar/testing/unit-test/payment/repository/xendit.go
@@ -5,47 +5,58 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
+type Config struct{
+	AuthKey string
+}
+
 type XenditPayment struct{
+	config Config
 	host string
 	httpConector HttpConector
 }
 
-func NewXenditClient(httpConector HttpConector, host string) XenditPayment {
+func NewXenditClient(httpConector HttpConector, host string, authKey string) XenditPayment {
 	return XenditPayment{
 		httpConector: httpConector,
 		host: host,
+		config: Config{
+			AuthKey: authKey,
+		},
 	}
 }
 
 type (
 	XenditPaymentReqBody struct {
-		Currency string 
-		Amount int
-		PaymentMethod PaymentMethod
-		Metadata Metadata
+		Currency string `json:"currency"`
+		Amount int 		`json:"amount"`
+		PaymentMethod PaymentMethod `json:"payment_method"`
+		Metadata Metadata `json:"metadata"`
 	}
 
 	PaymentMethod struct {
-		PaymentMethodType string 
-		Reusability string
-		ReferenceID string
-		VirtualAccount VirtualAccount
+		PaymentMethodType string `json:"type"`
+		Reusability string `json:"reusability"`
+		ReferenceID string `json:"reference_id"`
+		VirtualAccount VirtualAccount `json:"virtual_account"`
 	}
 
 	VirtualAccount struct {
-		ChannelCode string
-		ChannelProperties ChannelProperties
+		ChannelCode string `json:"channel_code"`
+		ChannelProperties ChannelProperties `json:"channel_properties"`
 	}
 
 	ChannelProperties struct{
-		CustomerName string
+		CustomerName string `json:"customer_name"`
 	}
 
 	Metadata struct {
-		SKU string
+		SKU string `json:"sku"`
 	}
 )
 func (x *XenditPayment) SendPaymentRequest(ctx context.Context) (paymentID string, err error) {
@@ -59,7 +70,24 @@ func (x *XenditPayment) SendPaymentRequest(ctx context.Context) (paymentID strin
 
 	// handle sucess response 
 
-	requestBody := XenditPaymentReqBody{}
+	requestBody := XenditPaymentReqBody{
+		Currency: "IDR",
+		Amount: 100000,
+		PaymentMethod: PaymentMethod{
+			PaymentMethodType: "VIRTUAL_ACCOUNT",
+			Reusability: "ONE_TIME",
+			ReferenceID: fmt.Sprintf("pm-level-%s", uuid.New().String()),
+			VirtualAccount: VirtualAccount{
+				ChannelCode: "BRI",
+				ChannelProperties: ChannelProperties{
+					CustomerName: "John Doe",
+				},
+			},
+		},
+		Metadata: Metadata{
+			SKU: "ABCDEFGH",
+		},
+	}
 	_ = requestBody
 	reqBody := new(bytes.Buffer)
 	err = json.NewEncoder(reqBody).Encode(requestBody)
@@ -70,11 +98,17 @@ func (x *XenditPayment) SendPaymentRequest(ctx context.Context) (paymentID strin
 
 	endpoint := fmt.Sprintf("%s%s",x.host, "/payment_requests")
 	httpReq, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, reqBody)
+	httpReq.Header.Set("Authorization", fmt.Sprintf("Basic %s", x.config.AuthKey))
+	httpReq.Header.Set("Content-Type", "application/json")
 
 	res, err := x.httpConector.Do(httpReq)
 	if err != nil {
 		return "", err
 	}
+
+	rawResponseBody, err := io.ReadAll(res.Body)
+	strResponseBody := string(rawResponseBody)
+	_ = strResponseBody
 
 	_ = res 
 	return "", err
--- dasar/testing/unit-test/payment/repository/xendit_test.go
+++ dasar/testing/unit-test/payment/repository/xendit_test.go
@@ -2,38 +2,39 @@ package repository
 
 import (
 	"context"
-	"errors"
 	"net/http"
 	"testing"
-	"unit-test/payment/repository/mock"
-
-	"github.com/stretchr/testify/assert"
-	"go.uber.org/mock/gomock"
 )
 
 // Negative Test Case
 // sending request with empty auth header
 // sending request with invalid auth header
 // sending request with valid auth header
-// sending request with empty request body
+// sending request with empty request body:
+// "{\"error_code\":\"API_VALIDATION_ERROR\",\"message\":\"amount must be greater than 0\"}\n"
+// "{\"error_code\":\"API_VALIDATION_ERROR\",\"message\":\"Only one of 'payment_method' or 'payment_method_id' should be present per request\"}\n"
 // sending request with broken request body
 // sending request with incomplete
 // sending request with invalid request body (missing some request field)
 // sending request with duplicate referenceID
-// sending request with inactive channel_code: BCA
 
 // Positive Test Case
+// "{\"id\":\"pr-1a12c5ec-cbfc-45bd-ad0b-51e699867ac1\",\"country\":\"ID\",\"amount\":100000,\"currency\":\"IDR\",\"business_id\":\"599bd7f1ccab55b020bb1147\",\"reference_id\":\"0d4cd7f0-30ef-45b0-a756-7f2194f04b3a\",\"payment_method\":{\"id\":\"pm-dcc65478-e017-45f0-b772-714c709fafac\",\"type\":\"VIRTUAL_ACCOUNT\",\"reference_id\":\"pm-level-c1a883c7-5e07-4024-ad74-3dc728341d2e\",\"description\":null,\"created\":\"2025-02-01T08:11:43.826892322Z\",\"updated\":\"2025-02-01T08:11:44.06893992Z\",\"card\":null,\"ewallet\":null,\"direct_debit\":null,\"direct_bank_transfer\":null,\"over_the_counter\":null,\"virtual_account\":{\"amount\":100000,\"currency\":\"IDR\",\"channel_code\":\"BRI\",\"channel_properties\":{\"customer_name\":\"John Doe\",\"virtual_account_number\":\"262158018439804\",\"expires_at\":\"2056-02-01T08:11:43.883582Z\"}},\"qr_code\":null,\"metadata\":null,\"billing_information\":{\"city\":null,\"country\":\"\",\"postal_code\":null,\"province_state\":null,\"street_line1\":null,\"street_line2\":null},\"reusability\":\"ONE_TIME_USE\",\"status\":\"PENDING\"},\"description\":null,\"metadata\":{\"sku\":\"ABCDEFGH\"},\"customer_id\":null,\"capture_method\":\"AUTOMATIC\",\"initiator\":null,\"card_verification_results\":null,\"created\":\"2025-02-01T08:11:43.772237142Z\",\"updated\":\"2025-02-01T08:11:43.772237142Z\",\"status\":\"PENDING\",\"actions\":[],\"failure_code\":null,\"channel_properties\":null,\"shipping_information\":null,\"items\":null}"
+
 // sending with complete and valid request body
+// sending with inactive channel_code : BCA
 
 // Edge Case
 // TODO: find the edge cases
 
 
-func TestXenditPayment_SendPaymentRequest_WithEmptyAuthHeader(t *testing.T){
+func TestXenditPayment_SendPaymentRequest_ApiExploration(t *testing.T){
 	httpClient := &http.Client{}
 	hostName := "https://api.xendit.co"
+	// apiKey := "something"
+	authKey := "eG5kX2RldmVsb3BtZW50X09vbUFmT1V0aCtHb3dzWTZMZUpPSHpMQ1p0U2o4NEo5a1hEbitSeGovbUhXK2J5aERRVnhoZz09Og=="
 
-	xenditClient := NewXenditClient(httpClient, hostName)
+	xenditClient := NewXenditClient(httpClient, hostName, authKey)
 	ctx := context.Background()
 	paymentID, err := xenditClient.SendPaymentRequest(ctx)
 	if err != nil {
@@ -65,15 +66,15 @@ func TestXenditPayment_SendPaymentRequest_SuccessResponse(t *testing.T){
 	
 }
 
-func TestXenditPayment_SendPaymentRequest(t *testing.T){
-	httpClientMock := mock.NewMockHttpConector(gomock.NewController(t))
-	host := "http://mock.server"
+// func TestXenditPayment_SendPaymentRequest(t *testing.T){
+// 	httpClientMock := mock.NewMockHttpConector(gomock.NewController(t))
+// 	host := "http://mock.server"
 
-	httpClientMock.EXPECT().Do(gomock.Any()).Return(nil, errors.New("something error on xendit end"))
+// 	httpClientMock.EXPECT().Do(gomock.Any()).Return(nil, errors.New("something error on xendit end"))
 	
-	xenditClient := NewXenditClient(httpClientMock, host)
-	paymentID, err := xenditClient.SendPaymentRequest(context.Background())
-	assert.Error(t, err, "it should not return error")
+// 	xenditClient := NewXenditClient(httpClientMock, host)
+// 	paymentID, err := xenditClient.SendPaymentRequest(context.Background())
+// 	assert.Error(t, err, "it should not return error")
 
-	assert.Empty(t, paymentID, "it should return a valid created paymentID")
-}
+// 	assert.Empty(t, paymentID, "it should return a valid created paymentID")
+// }
