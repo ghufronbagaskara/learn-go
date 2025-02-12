e11ab262f29d8a9b57c15f399f3186ae2a381510
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2025-02-12 21:22:25 +0700

    feat@grpc: adding client, succesfully implement communication

--- dasar/grpc/client/main.go
+++ dasar/grpc/client/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"context"
+	pb "grpc-example/proto"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 func main() {
-	
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	r, err := client.SayHello(ctx, &pb.HelloRequest{
+		Name: "Azzachra",
+	})
+
+	if err != nil {
+		log.Printf("an error occured: %v", err)
+		return
+	}
+
+	log.Printf("Message from server: %s", r.GetMessage())
 }
